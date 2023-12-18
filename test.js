9a8d495d5d34c3221acb3b73b8f70c8ac25e7ba9
ctabere <c.taber.edwards@gmail.com>
2023-12-18 14:34:55 -0500

    123

---
+++ test.js
@@ -0,0 +1,7 @@
+// const example_object = require("./functions/example_object.js");
+
+var bab = "shshsh";
+const car= {name: "jonny", age : 3};
+console.log(`this is any text!
+and you cna create line breaks !
+also give out vars: ${bab} + ${car.name} age: ${car.age}`);
